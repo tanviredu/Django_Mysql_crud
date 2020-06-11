be77af4e9f3e6e78284853184fc21cf726eb365e
tanviredu <ornobtanvir.git@gmail.com>
2020-06-11 15:51:24 +0600

    added

--- CLASS_BASED_VIEW/TEST/models.py
+++ CLASS_BASED_VIEW/TEST/models.py
@@ -1,1 +1,27 @@
 from django.db import models
+
+class Musician(models.Model):
+    first_name     = models.CharField(max_length=150)
+    last_name      = models.CharField(max_length=150)
+    instrument     = models.CharField(max_length=150)
+    
+    def __str__(self):
+        return self.first_name + " " + self.last_name
+    
+
+class Album(models.Model):
+    artist         = models.ForeignKey(Musician,on_delete=models.CASCADE)
+    name           = models.CharField(max_length=150)
+    release_date   = models.DateField()
+    
+    rating = (
+        (1,"Very Bad"),
+        (2,"Bad"),
+        (3,"Good"),
+        (4,"Very Good"),
+        (5,"Excelent!! "),
+        )
+    num_star = models.IntegerField(choices=rating)
+    
+    def __str__(self):
+        return "{} Album Name : {}".format(self.artist,self.name)
